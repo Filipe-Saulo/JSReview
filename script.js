83accd8f4f3bc8419a4478ce399388417d3bba0b
Filipe <filipeunithing@gmail.com>
2024-09-04 10:56:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -284,4 +284,17 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 );
 booksAfterUpdate;
 
+//async - await
+
+//metodo then, passar dentro dos () o que sera executado assim que os dados "chegarem"
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+getTodos();
 //ATENCAO - Utilizar VSCode extensão Quokka.js - ativar extensao: >quokka.js: Start on Current file
