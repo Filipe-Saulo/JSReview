7e93c0df2784a9d0c3c1e41bf3d4f3171d3327ad
Filipe <filipeunithing@gmail.com>
2024-09-04 09:38:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -243,3 +243,14 @@ const essentialData = books.map((book) => ({
   author: book.author,
 }));
 console.log(essentialData);
+
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((books) => books.genres.includes("adventure"))
+  .map((book) => book.title);
+
+adventureBooks;
